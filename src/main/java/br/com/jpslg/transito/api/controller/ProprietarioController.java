3e41eedf520766cb3814eda77a99b9a9aa2b867f
JonathanGuimarae3s <guimaraesjonathan92@gmail.com>
2024-12-11 23:37:47 -0300

    Implementando endpoint de exclusão de recurso

--- src/main/java/br/com/jpslg/transito/api/controller/ProprietarioController.java
+++ src/main/java/br/com/jpslg/transito/api/controller/ProprietarioController.java
@@ -4,20 +4,71 @@ import br.com.jpslg.transito.domain.model.Proprietario;
 import br.com.jpslg.transito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
+
 
     private ProprietarioRepository proprietarioRepository;
 
-    @GetMapping("/proprietarios")
+    @GetMapping
     public List<Proprietario> retornaTodosProprietarios() {
         return proprietarioRepository.findAll();
     }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> retornaProprietarioPorId(@PathVariable Long proprietarioId) {
+        Optional<Proprietario> proprietario = proprietarioRepository.findById(proprietarioId);
+
+        return proprietario.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionaProprietario(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
+    }
+
+    @PutMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId, @RequestBody Proprietario proprietario) {
+
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietario.setId(proprietarioId);
+
+        Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
+
+        return ResponseEntity.ok(proprietarioAtualizado);
+    }
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> deletar(@PathVariable Long proprietarioId) {
+
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            ResponseEntity.notFound().build();
+        }
+
+        proprietarioRepository.deleteById(proprietarioId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+    /*@GetMapping("/{proprietarioNome}")
+    public List<Proprietario> retornaProprietarioPorNome(@PathVariable String proprietarioNome) {
+        return proprietarioRepository.findByNomeContaining(proprietarioNome);
+    }*/
 
 }
--- src/main/java/br/com/jpslg/transito/domain/repository/ProprietarioRepository.java
+++ src/main/java/br/com/jpslg/transito/domain/repository/ProprietarioRepository.java
@@ -4,9 +4,12 @@ import br.com.jpslg.transito.domain.model.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProprietarioRepository extends JpaRepository<Proprietario,Long> {
+public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
 
 
+    List<Proprietario> findByNomeContaining(String nome);
 
 }
